314b518c3c239619072e3dc49c4a5a839c029837
Pablo de la Pena <p@shedcollective.org>
2012-11-09 10:24:22 +0000

    Including nails.default.css by default
    So we're more likely to produce better websites and so that common utility styles are available

--- application/core/NAILS_Controller.php
+++ application/core/NAILS_Controller.php
@@ -10,7 +10,25 @@
 /* load the class from the package */
 require NAILS_PATH . 'core/CORE_NAILS_Controller.php';
 
-class NAILS_Controller extends CORE_NAILS_Controller {}
+class NAILS_Controller extends CORE_NAILS_Controller {
+	
+	
+	public function __construct()
+	{
+		parent::__construct();
+		
+		// --------------------------------------------------------------------------
+		
+		//	Load NAILS. Assets; this little nugget of CSS will pull in skeleton and
+		//	some common utility styles, such as Super Awesome Buttons.
+		
+		if ( $this->uri->segment( 1 ) != 'admin' ) :
+		
+			$this->asset->load( 'nails.default.css', TRUE );
+			
+		endif;
+	}
+}
 
 /* End of file NAILS_Controller.php */
 /* Location: ./public_html/application/core/NAILS_Controller.php */
