f085e797d1ded6c756a04b9b7768b4e9efc15df2
Pablo de la Pena <p@shedcollective.org>
2013-04-17 17:32:20 +0100

    Updated db_user.sql to allow for multiple initial users

--- _temp/db_user.sql
+++ _temp/db_user.sql
@@ -1,6 +1,6 @@
-INSERT INTO `user` (`id`, `id_md5`, `auth_method_id`, `group_id`, `ip_address`, `last_ip`, `password`, `password_md5`, `salt`, `email`, `created`, `last_update`)
+INSERT INTO `user` (`id`, `id_md5`, `auth_method_id`, `group_id`, `ip_address`, `last_ip`, `password`, `password_md5`, `salt`, `email`, `created`, `last_update`, `temp_pw`)
 VALUES
-	(1, 'c4ca4238a0b923820dcc509a6f75849b', {{VALUE_USER_AUTH_METHOD}}, {{VALUE_USER_GROUP_ID}}, '127.0.0.1', '127.0.0.1', '{{VALUE_USER_PASSWORD}}', '{{VALUE_USER_PASSWORD_MD5}}', '{{VALUE_USER_SALT}}', '{{VALUE_USER_EMAIL}}', NOW(), NOW());
+	({{VALUE_USER_ID}}, {{VALUE_USER_ID_MD5}}, {{VALUE_USER_AUTH_METHOD}}, {{VALUE_USER_GROUP_ID}}, '127.0.0.1', '127.0.0.1', '{{VALUE_USER_PASSWORD}}', '{{VALUE_USER_PASSWORD_MD5}}', '{{VALUE_USER_SALT}}', '{{VALUE_USER_EMAIL}}', NOW(), NOW(), 1);
 INSERT INTO `user_meta` (`user_id`,`first_name`, `last_name`)
 VALUES
-	(1, '{{VALUE_USER_FIRST_NAME}}', '{{VALUE_USER_LAST_NAME}}');
+	({{VALUE_USER_ID}}, '{{VALUE_USER_FIRST_NAME}}', '{{VALUE_USER_LAST_NAME}}');
