49bac0763a45febba4a06b591ea1af00a281a96f
Pablo de la Pena <p@shedcollective.org>
2013-10-07 22:23:45 +0100

    Not loading Nails assets by default in the app.

--- application/core/NAILS_Controller.php
+++ application/core/NAILS_Controller.php
@@ -2,7 +2,7 @@
 
 /**
  * Nails. From the Shed
- * 
+ *
  * @package		Nails
  * @author		Pablo
  */
@@ -12,18 +12,6 @@ require NAILS_PATH . 'core/CORE_NAILS_Controller.php';
 
 class NAILS_Controller extends CORE_NAILS_Controller
 {
-	public function __construct()
-	{
-		parent::__construct();
-		
-		// --------------------------------------------------------------------------
-		
-		//	Load NAILS. Assets; this little nugget of CSS will pull in skeleton and
-		//	some common utility styles, such as Super Awesome Buttons.
-		
-		$this->asset->load( 'nails.default.css', TRUE );
-		$this->asset->load( 'nails.default.min.js', TRUE );
-	}
 }
 
 /* End of file NAILS_Controller.php */
