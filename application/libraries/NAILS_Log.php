bb86b8306f70b97994a8d8555bbf3123e43cd7d0
Pablo de la Pena <p@shedcollective.org>
2014-07-03 00:03:46 +0100

    Added NAILS_Email which calls common/libraries/CORE_NAILS_EMAIL.php

--- application/libraries/NAILS_Log.php
+++ application/libraries/NAILS_Log.php
@@ -12,5 +12,5 @@ require_once NAILS_COMMON_PATH . 'libraries/CORE_NAILS_Log.php';
 
 class NAILS_Log extends CORE_NAILS_Log {}
 
-/* End of file NAILS_Logs.php */
-/* Location: ./application/libraries/NAILS_Logs.php */
+/* End of file NAILS_Log.php */
+/* Location: ./application/libraries/NAILS_Log.php */
