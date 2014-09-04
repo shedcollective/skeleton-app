42a3187fdb6937277cd6ec28d8e65b98b5762d56
Pablo de la Pena <p@shedcollective.org>
2014-09-04 20:03:33 +0100

    New Favicon and replaced console apps with single touchpoint

---
+++ nails.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env php
+<?php
+
+/**
+ * ---------------------------------------------------------------
+ * NAILS CONSOLE
+ * ---------------------------------------------------------------
+ *
+ * This is the console application for Nails.
+ *
+ * Lead Developer: Pablo de la Peña	(p@shedcollective.org, @hellopablo)
+ * Lead Developer: Gary Duncan		(g@shedcollective.org, @gsdd)
+ *
+ * Documentation: http://nailsapp.co.uk/console
+ */
+
+require_once dirname(__FILE__) . '/vendor/nailsapp/common/console/console.php';
+
+/* End of file nails.php */
+/* Location: ./nails.php */
