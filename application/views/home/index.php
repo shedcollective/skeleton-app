250cc3889d58b7a0d33935c6dfe275a4b718ce8d
Pablo de la Peña <p@shedcollective.org>
2015-01-13 01:13:47 +0000

    Better default template

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,86 +1,78 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title><?=APP_NAME?></title>
-    <style type="text/css">
+<style type="text/css">
 
-        html,
-        body {
-            -ms-text-size-adjust: 100%;
-            -webkit-text-size-adjust: 100%;
-            color: #333;
-            overflow: hidden;
-            font-family: "Helvetica Neue", Helvetica, "Segoe UI", Arial, freesans, sans-serif;
-            font-size: 16px;
-            line-height: 1.6em;
-        }
+    html,
+    body {
+        -ms-text-size-adjust: 100%;
+        -webkit-text-size-adjust: 100%;
+        color: #333;
+        overflow: hidden;
+        font-family: "Helvetica Neue", Helvetica, "Segoe UI", Arial, freesans, sans-serif;
+        font-size: 16px;
+        line-height: 1.6em;
+    }
 
-        #container {
-            word-wrap: break-word;
-            max-width: 600px;
-            min-width: 200px;
-            margin: 0 auto;
-            padding: 30px;
-            text-align: center;
-        }
+    #container {
+        word-wrap: break-word;
+        max-width: 600px;
+        min-width: 200px;
+        margin: 0 auto;
+        padding: 30px;
+        text-align: center;
+    }
 
-        h1 {
-            position: relative;
-            margin: 0.67em 0;
-            margin-top: 1em;
-            margin-bottom: 16px;
-            padding-bottom: 0.3em;
-            font-size: 2.25em;
-            font-weight: bold;
-            line-height: 1.2;
-            border-bottom: 1px solid #eee;
-        }
+    h1 {
+        position: relative;
+        margin: 0.67em 0;
+        margin-top: 1em;
+        margin-bottom: 16px;
+        padding-bottom: 0.3em;
+        font-size: 2.25em;
+        font-weight: bold;
+        line-height: 1.2;
+        border-bottom: 1px solid #eee;
+    }
 
-        img {
-            border: 0;
-            max-width: 100%;
-            -moz-box-sizing: border-box;
-            box-sizing: border-box;
-        }
+    img {
+        border: 0;
+        max-width: 100%;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+    }
 
-        p {
-            margin-top: 0;
-            margin-bottom: 16px;
-        }
+    p {
+        margin-top: 0;
+        margin-bottom: 16px;
+    }
 
-        a {
-            background: transparent;
-            color: #4183c4;
-            text-decoration: none;
-        }
+    a {
+        background: transparent;
+        color: #4183c4;
+        text-decoration: none;
+    }
 
-        a:active,
-        a:hover {
-            outline: 0;
-        }
+    a:active,
+    a:hover {
+        outline: 0;
+    }
 
-        a:hover,
-        a:focus,
-        a:active {
-            text-decoration: underline;
-        }
+    a:hover,
+    a:focus,
+    a:active {
+        text-decoration: underline;
+    }
 
-    </style>
-</head>
-<body>
-    <div id="container">
-        <h1>
-            <img src="assets/img/nails-icon.png" width="125" height="125" />
-        </h1>
-        <p>
-            You did it, you got yourself set up with a new Nails app. That was easy,
-            wasn't it?
-        </p>
-        <p>
-            The next step is to get stuck in and start coding something awesome.
-            The <a href="http://docs.nailsapp.co.uk" target="_blank">documentation</a>
-            is probably a good place to start.
-        </p>
-    </div>
-</body>
-</html>
+</style>
+<div id="container">
+<h1>
+    <img src="assets/img/nails-icon.png" width="125" height="125" />
+</h1>
+<p>
+    You did it, you got yourself set up with a new Nails app. That was easy,
+    wasn't it?
+</p>
+<p>
+    The next step is to get stuck in and start coding something awesome.
+    The <a href="http://docs.nailsapp.co.uk" target="_blank">documentation</a>
+    is probably a good place to start.
+</p>
+</div>
