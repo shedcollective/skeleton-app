5db110d3cd5381b49e0d6de032632a56331a8fcb
Pablo de la Pena <p@shedcollective.org>
2015-01-27 01:04:42 +0000

    Tweaks
    - A little more guidance for setting up a new Nails site
    - Some sample files
    - robots.txt to ignore the vendor folder
    - Added bootstrap as a bower dependency (default views expect it, but shouldn't have it hard coded

--- application/core/NAILS_Controller.php
+++ application/core/NAILS_Controller.php
@@ -5,4 +5,32 @@ require_once NAILS_COMMON_PATH . 'core/CORE_NAILS_Controller.php';
 
 class NAILS_Controller extends CORE_NAILS_Controller
 {
+    /**
+     * Construct the controller
+     * Code that is defined in here is executed before the controller which
+     * is being called by the URL. It is common to load site-wide assets in
+     * here, or to define site-wide variables.
+     */
+    public function __construct()
+    {
+        /**
+         * It is important to call the parent constructor in order to ensure
+         * expected functionality is inherited properly.
+         */
+
+        parent::__construct();
+
+        //  Load CSS from the App's `assets/bower_components` directory
+        $this->asset->load('bootstrap/dist/css/bootstrap.min.css', 'BOWER');
+
+        //  Load CSS from the App's `assets/css` directory
+        $this->asset->load('styles.css');
+
+        //  Load JS from the App's `assets/bower_components` directory
+        $this->asset->load('jquery/dist/jquery.min.css', 'BOWER');
+        $this->asset->load('bootstrap/dist/js/bootstrap.min.css', 'BOWER');
+
+        //  Load JS from the App's `assets/js` directory
+        $this->asset->load('app.min.js');
+    }
 }
