6a9533a48666b84407423c9873e3ca452fb74773
Carlos Reig Matut <unstatu@gmail.com>
2017-05-30 10:57:33 +0200

    Added a the custom library for form validation

---
+++ application/libraries/NAILS_Form_validation.php
@@ -0,0 +1,8 @@
+<?php
+
+/* load the class from the package */
+require_once NAILS_COMMON_PATH . 'libraries/CORE_NAILS_Form_validation.php';
+
+class NAILS_Form_validation extends CORE_NAILS_Form_validation
+{
+}
