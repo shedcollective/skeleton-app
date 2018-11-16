fb7700d0a41c69d8b9eede12bb9a40dc52aca2da
Pablo de la Peña <p@shedcollective.org>
2018-11-16 16:33:28 +0000

    Improved testing environment

---
+++ tests/Unit/README.md
@@ -0,0 +1,3 @@
+# Testing: Unit
+
+The tests in this testing suite are intended to test small, isolated portions of the application, e.g. individual methods, functions, or classes. 
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,25 @@
 <?php
 
-require 'vendor/autoload.php';
+/**
+ * ---------------------------------------------------------------
+ * NAILS MAIN APPLICATION
+ * ---------------------------------------------------------------
+ *
+ * This is the kick off point for the main Nails Application.
+ * Documentation: https://nailsapp.co.uk
+ */
+
+/*
+ *---------------------------------------------------------------
+ * Autoloader
+ *---------------------------------------------------------------
+ */
+$sEntryPoint = realpath(__DIR__ . '/..') . '/index.php';
+require_once dirname($sEntryPoint) . '/vendor/autoload.php';
+
+/*
+ *---------------------------------------------------------------
+ * Nails Bootstrapper
+ *---------------------------------------------------------------
+ */
+\Nails\Bootstrap::run($sEntryPoint);
