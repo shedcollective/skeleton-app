8f320137b92f70a4f170d678eaa89a2cfb20bcc8
Pablo de la Peña <p@shedcollective.org>
2019-02-11 17:41:37 +0000

    style: Reformatting the services.php file

--- application/services/services.php
+++ application/services/services.php
@@ -2,34 +2,35 @@
 
 /**
  * Define your application's services, models and factories.
+ *
  * @link http://nailsapp.co.uk/docs/services
  */
 
-return array(
+return [
 
 
     /**
      * Classes/libraries which don't necessarily relate to a database table.
      * Once instantiated, a request for a service will always return the same instance.
      */
-    'services' => array(
+    'services'  => [
         // GENERATOR[SERVICES]
-    ),
+    ],
 
 
     /**
      * Models generally represent database tables.
      * Once instantiated, a request for a model will always return the same instance.
      */
-    'models' => array(
+    'models'    => [
         // GENERATOR[MODELS]
-    ),
+    ],
 
 
     /**
      * A class for which a new instance is created each time it is requested.
      */
-    'factories' => array(
+    'factories' => [
         // GENERATOR[FACTORIES]
-    )
-);
+    ],
+];
